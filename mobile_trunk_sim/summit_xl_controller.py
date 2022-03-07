9fa3b4f2b13693eae93809bde8601b3d1c36aa72
Fabrice DEDO <dsamfabrice5@gmail.com>
2022-03-07 14:35:42 +0100

    Updated summit_xl controller to be consistent with ros controller

--- mobile_trunk_sim/summit_xl_controller.py
+++ mobile_trunk_sim/summit_xl_controller.py
@@ -13,11 +13,15 @@ class SummitxlController(Sofa.Core.Controller):
     
         Sofa.Core.Controller.__init__(self, *args, **kwargs)
 
-        self.move_forward = 0.005
-        self.move_backward = -0.005
+        speed = 1
+        ray = 0.001 #rayon de la roue
+        dx = 2*ray*pi # deplacement pour un tour de roue
+        w = speed/ray #rotation des roues
+        self.move_forward = dx
+        self.move_backward = -dx
         self.wheels = kwargs["wheels"]
         self.chassis = kwargs["chassis"]
-        self.angle = pi/64
+        self.angle = w
 
     def onKeypressedEvent(self, event):
         key = event['key']
@@ -39,16 +43,16 @@ class SummitxlController(Sofa.Core.Controller):
                 wheel_rigid.translate([self.move_forward, 0.0, 0.0])
                 wheel_rigid.rotateAround([0, 1, 0],self.angle)
         
-        elif key == Key.uparrow:
+        elif key == Key.uparrow: 
             chassis_rigid = RigidDof(self.chassis.dofs)
-            chassis_rigid.rotateAround([0, 0, 1],self.angle)
+            #chassis_rigid.rotateAround([0, 0, 1],self.angle)
             for i in range(0,4):
                 wheel_rigid = RigidDof(self.wheels[i].dofs)
                 wheel_rigid.rotateAround([0, 0, 1],self.angle)
 
         elif key == Key.downarrow:
             chassis_rigid = RigidDof(self.chassis.dofs)
-            chassis_rigid.rotateAround([0, 0, 1],-self.angle)
+            #chassis_rigid.rotateAround([0, 0, 1],-self.angle)
             for i in range(0,4):
                 wheel_rigid = RigidDof(self.wheels[i].dofs)
                 wheel_rigid.rotateAround([0, 0, 1],-self.angle)
