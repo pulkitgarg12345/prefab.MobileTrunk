ae6592c98531ed11ec78469d417565120bc3005b
Fabrice DEDO <dsamfabrice5@gmail.com>
2022-03-07 14:18:48 +0100

    Add a ros controller for the summit_xl

--- mobile_trunk_sim/summit_xl_ros_controller.py
+++ mobile_trunk_sim/summit_xl_ros_controller.py
@@ -1,23 +1,38 @@
 # coding: utf8
 #!/usr/bin/env python3
-import Sofa
-import sofaros
-from sofaros import *
-from splib3.numerics import RigidDof, Quat
-from splib3.animation import animate
-from splib3.constants import Key
-from stlib3.scene import Scene
-from math import *
-from std_msgs.msg import Float32MultiArray 
-from summit_xl_controller import *
 
+import Sofa
+from splib3.numerics import RigidDof
+from std_msgs.msg import Float32MultiArray 
+from math import *
+
+global ray , speed
+speed = 1
+ray = 0.001 #rayon de la roue
+dx = 2*ray*pi # deplacement pour un tour de roue
+w = speed/ray
 
 def send(data):
     msg = Float32MultiArray()
     msg.data = list(data.value[0])
     return msg  
 
-
 def recv(data, datafield):
-    pass
+    t = data.tolist()
+    datafield.value = [[t[0] + dx, t[1], t[2], t[3], t[4],
+                        t[5], t[6]]]
 
+class SummitxlrosController(Sofa.Core.Controller):
+    def __init__(self, *args, **kwargs):
+        Sofa.Core.Controller.__init__(self, *args, **kwargs)
+        self.chassis = kwargs["chassis"]
+        self.robot = kwargs["robot"]
+        self.wheels = kwargs["wheels"]
+
+    def onAnimateBeginEvent(self, event):
+        self.chassis.dofs.position = self.robot.dofs.position
+        print(self.chassis.dofs.position.value[0][0])
+        for i in range(0,4):
+            wheel_rigid = RigidDof(self.wheels[i].dofs)
+            wheel_rigid.translate([dx, 0.0, 0.0])
+            wheel_rigid.rotateAround([0, 1, 0], w)
---
+++ mobile_trunk_sim/test_ros_controller.py
@@ -0,0 +1,121 @@
+import sys
+import Sofa
+from stlib3.scene import Scene
+from splib3.numerics import Quat
+from math import pi
+import sofaros
+from sofaros import *
+from std_msgs.msg import Float32MultiArray 
+from summit_xl_ros_controller import *
+
+front_left_wheel_position = [0.229, 0.235, 0.0]
+back_left_wheel_position = [-0.229, 0.235, 0.0]
+front_right_wheel_position = [0.229, -0.235, 0.0]
+back_right_wheel_position = [-0.229, -0.235, 0.0]
+
+front_left_wheel_orientation = [0.0, 0.0, 0.0, 0.0]
+front_right_wheel_orientation = [0.0, 0.0, 0.0, 0.0]
+back_left_wheel_orientation = [0.0, 0.0, 0.0, 0.0]
+back_right_wheel_orientation = [0.0, 0.0, 0.0, 0.0]
+
+rosNode = sofaros.init("SofaNode")
+
+
+
+
+def wheel_orientation(angle):
+    wheel_orientation = [0.0, 0.0, 0.0, 0.0]
+    wheel_orientation = Quat.createFromAxisAngle([1.0, 0., 0], angle)
+    wheel_orientation.rotateFromEuler([angle, 0. , 0.])
+    
+    return wheel_orientation
+
+def Floor(parentNode, color=[0.5, 0.5, 0.5, 1.], rotation=[0, 0, 0], 
+          position=[0.0, 0.0, 0, 0.0, 0.0, 0.0, 1.], translation=[-1, -1, -0.15]):
+    floor = parentNode.addChild('Floor')
+    floor.addObject('MeshObjLoader', name='loader', filename='mesh/square1.obj', scale=2, rotation=rotation, translation=translation)
+    floor.addObject('OglModel', src='@loader', color=color)
+    floor.addObject('MeshTopology', src='@loader', name='topo')
+    floor.addObject('MechanicalObject')
+    floor.addObject('TriangleCollisionModel')
+    floor.addObject('LineCollisionModel')
+    floor.addObject('PointCollisionModel')
+    return floor
+
+def createWheel(parent, name, wheel_position, wheel_orientation):
+    body = parent.addChild(name)
+    body.addObject('MechanicalObject', name='dofs', showObject=True, template='Rigid3',
+                    position=[wheel_position[0], wheel_position[1], wheel_position[2],
+                              wheel_orientation[0], wheel_orientation[1], wheel_orientation[2],
+                              wheel_orientation[3]],showObjectScale=0.09)
+    visual = body.addChild('VisualModel')
+
+    visual.addObject('MeshSTLLoader', name='loader1', filename='meshes/wheel.stl')
+
+    visual.addObject('MeshTopology', src='@loader1')
+
+    visual.addObject('OglModel', name='renderer', src='@loader1', color=[0.15, 0.45, 0.75, 0.7])
+
+    visual.addObject('RigidMapping',
+                        input=body.dofs.getLinkPath(),
+                        output=visual.renderer.getLinkPath())
+
+    return body
+
+def createScene(rootNode):
+    rootNode.addObject('DefaultVisualManagerLoop')
+    rootNode.addObject('DefaultAnimationLoop')
+  
+    rootNode.dt = 0.01
+    rootNode.gravity = [0., -9810., 0.]
+
+    robot = rootNode.addChild("Summit_xl")    
+    chassis = robot.addChild("Chassis")
+    robot.addObject('MechanicalObject', name = 'dofs', template ='Rigid3', position=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.], showObject=True, showObjectScale=0.09)
+    
+    chassis.addObject('MechanicalObject', name='dofs', template='Rigid3',
+                             position=[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.],
+                             showObject=False, showObjectScale=0.09)
+    
+    visual = chassis.addChild("VisualModel")
+    visual.addObject('MeshSTLLoader', name='loader', filename='meshes/summit_xl_chassis_simple.stl')
+    visual.addObject('MeshTopology', src='@loader')
+    visual.addObject('OglModel', name='renderer',
+                        src='@loader',
+                        color=[0.6, 0.6, 0.6, 0.6])
+    visual.addObject('RigidMapping',
+                        input=chassis.dofs.getLinkPath(),
+                        output=visual.renderer.getLinkPath())
+    
+    front_right_wheel_orientation = wheel_orientation(pi/2)
+   
+    back_right_wheel_orientation = wheel_orientation(pi/2)
+    
+    front_left_wheel_orientation = wheel_orientation(pi)
+   
+    back_left_wheel_orientation = wheel_orientation(pi)
+    
+    wheel1 = createWheel(robot, 'front_left_wheel',
+                         front_left_wheel_position, front_left_wheel_orientation)
+    
+    wheel2 = createWheel(robot, 'back_left_wheel',
+                         back_left_wheel_position, back_left_wheel_orientation)
+    
+    wheel3 = createWheel(robot, 'front_right_wheel',
+                         front_right_wheel_position, front_right_wheel_orientation)
+    
+    wheel4 = createWheel(robot, 'back_right_wheel',
+                         back_right_wheel_position, back_right_wheel_orientation)
+
+    floor = Floor(rootNode)
+
+    rootNode.addObject(sofaros.RosReceiver(rosNode, "/summit_xl_control/cmd_vel",
+                                           robot.dofs.findData("position"), Float32MultiArray, recv))
+
+    rootNode.addObject(sofaros.RosSender(rosNode, "/summit_xl_control/cmd_vel",
+                                           robot.dofs.findData("position"), Float32MultiArray, send))
+    
+
+    robot.addObject(SummitxlrosController(rootNode, chassis=chassis, robot=robot,wheels = [wheel1, wheel2, wheel3, wheel4]))
+
+    return rootNode
