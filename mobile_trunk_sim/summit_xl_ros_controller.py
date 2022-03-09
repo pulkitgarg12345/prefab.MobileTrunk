3f06f28b65dd8e6776ec83dd0531525959b8f632
Fabrice DEDO <dsamfabrice5@gmail.com>
2022-03-09 16:17:40 +0100

    Updated ros controller

--- mobile_trunk_sim/summit_xl_ros_controller.py
+++ mobile_trunk_sim/summit_xl_ros_controller.py
@@ -13,14 +13,9 @@ ray = 0.0015 #rayon de la roue
 dx = 2*ray*pi # deplacement pour un tour de roue
 w = speed/ray #rotation des roues
 
-def send(data):
-    msg = Float32MultiArray()
-    msg.data = list(data.value[0])
-    return msg  
-
 def recv(data, datafield):
     t = data.tolist()
-    datafield.value = [[t[0] + dx, t[1], t[2], t[3], t[4],
+    datafield.value = [[t[0], t[1], t[2], t[3], t[4],
                         t[5], t[6]]]
 
 class SummitxlrosController(Sofa.Core.Controller):
@@ -29,21 +24,20 @@ class SummitxlrosController(Sofa.Core.Controller):
         self.chassis = kwargs["chassis"]
         self.robot = kwargs["robot"]
         self.wheels = kwargs["wheels"]
+        self.ray = 0.0015
+        self.dt = None
+        self.dx = 0
+        self.speed = 1
+        self.w = 0
 
-    def onAnimateEndEvent(self, event):
+    def onAnimateBeginEvent(self, event):
+        self.dt = event['dt']
+        self.dx = self.robot.velocity[0] * self.dt
+        self.robot.dofs.position[0][0]+= self.dx #mettre à jour la position
+        #écrire un scrpit ros qui vient mettre à jour la commande vitesse
+        self.w = self.speed/self.ray
         self.chassis.dofs.position = self.robot.dofs.position
         for i in range(0,4):
             wheel_rigid = RigidDof(self.wheels[i].dofs)
-            wheel_rigid.translate([dx, 0.0, 0.0])
-            wheel_rigid.rotateAround([0, 1, 0], w)
-    
-    def onKeypressedEvent(self, event):
-        global speed, w
-        key = event['key']
-        if key == Key.minus:
-            speed-=0.1
-            if speed < 1:
-                speed = 1
-        elif key == Key.plus:
-            speed+=0.1
-        w = speed/ray #mis à jour de la vitesse angulaire
+            wheel_rigid.translate([self.dx , 0.0, 0.0])
+            wheel_rigid.rotateAround([0, 1, 0], self.w)
