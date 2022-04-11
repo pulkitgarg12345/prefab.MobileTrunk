5105dbe843936a9dbc9fa1e2cc0d3dd7998b6038
Fabrice DEDO <dsamfabrice5@gmail.com>
2022-04-11 22:55:53 +0200

    Changed the size of the vector that contains the message to be published from 3d to 6d

--- mobile_trunk_sim/ros_publisher.py
+++ mobile_trunk_sim/ros_publisher.py
@@ -13,7 +13,10 @@ class MinimalPublisher(Node):
         self.timer = self.create_timer(timer_period, self.timer_callback)
 
     def timer_callback(self):
-        msg = Float32MultiArray(layout=std_msgs.msg.MultiArrayLayout(dim=[], data_offset=0), data=[-10, 3.0, 0.1])
+        linear = [0, 1, 1]
+        angular = [0, 0, 0]
+        msg = Float32MultiArray(layout=std_msgs.msg.MultiArrayLayout( data_offset=0), data=[linear[0],linear[1],
+                                                                    linear[2],angular[0],angular[1],angular[2]])
         self.publisher_.publish(msg)
 
 
