5d6a231539faecbad574cfbbbfb0db6226ec7b1f
samFabrice <dsamfabrice5@gmail.com>
2022-07-11 13:29:16 +0200

    Integration of the trunk into the ros model of the robot

--- mobile_trunk_sim/ros_summitxl.py
+++ mobile_trunk_sim/ros_summitxl.py
@@ -1,8 +1,5 @@
-from turtle import position
 import Sofa
-from stlib3.scene import Scene
-from splib3.numerics import Quat
-from math import pi
+from stlib3.scene import Scene, ContactHeader
 import sofaros
 from sofaros import *
 from geometry_msgs.msg import Twist
@@ -10,18 +7,49 @@ from sensor_msgs.msg import Imu
 from summit_xl import SummitXL, Floor
 from summitxl_roscontroller import *
 from nav_msgs.msg import Odometry
+from echelon3.parameters import *
+from echelon3.createEchelon import *
+
 
 rosNode = sofaros.init("SofaNode")
 
 
 def createScene(rootNode):
+    ContactHeader(rootNode, alarmDistance=0.5*1000, contactDistance=0.07*1000)
+
+    #########################################
+    # Plugins, data and Solvers
+    ######################################### 
+
+    rootNode.addObject('OglSceneFrame', style="Arrows", alignment="TopRight");
+    rootNode.addObject('RequiredPlugin', name='SofaPython3')
+    rootNode.addObject('RequiredPlugin', name='BeamAdapter')
+    rootNode.addObject('RequiredPlugin', name='SoftRobots')
+    rootNode.addObject('RequiredPlugin', name='SofaMeshCollision')
+    rootNode.addObject('RequiredPlugin', name='SofaPlugins', pluginName='SofaGeneralRigid SofaGeneralEngine SofaConstraint SofaImplicitOdeSolver SofaSparseSolver SofaDeformable SofaEngine SofaBoundaryCondition SofaRigid SofaTopologyMapping SofaOpenglVisual SofaMiscCollision')
+
     scene = Scene(rootNode)
     scene.addMainHeader()
-    scene.dt = 0.01
+    scene.VisualStyle.displayFlags = 'showCollisionModels showForceFields'
+    scene.addObject('DefaultVisualManagerLoop')
+    scene.dt = 0.001
     scene.gravity = [0., -9810., 0.]
 
+    scene.Modelling.addObject('EulerImplicitSolver')
+    solver = scene.Modelling.addObject('SparseLDLSolver',name = 'SparseLDLSolver',template="CompressedRowSparseMatrixMat3x3d")
+    scene.Modelling.addObject('GenericConstraintCorrection' , solverName='SparseLDLSolver')
+    
+    #########################################
+    # create summit
+    #########################################
+
     SummitXL(scene.Modelling)
-    Floor(scene.Modelling, rotation=[90,0,0], translation=[-2,-0.12,-2], scale=4)
+    floor = Floor(rootNode,
+                  name="Floor",
+                  translation=[-2*1000, -0.12*1000, -2*1000],
+                  uniformScale=0.3*1000,
+                  isAStaticObject=True)
+
     robot=scene.Modelling.SummitXL
     scene.Modelling.SummitXL.addObject(SummitxlROSController(name="KeyboardController", robot=scene.Modelling.SummitXL))
 
@@ -47,6 +75,15 @@ def createScene(rootNode):
     scene.Modelling.SummitXL.addObject(sofaros.RosSender(rosNode, "/sofa_sim/cmd_vel",
                                            [robot.findData('robot_linear_vel'),robot.findData('robot_angular_vel')],
                                            Twist, vel_send))
+
+    ########################################
+    # createEchelon
+    ######################################## 
+
+    arm = rootNode.Modelling.SummitXL.Chassis.addChild('Arm')
+    connection = rootNode.Modelling.SummitXL.Chassis.Sensors.position
+    createEchelon(arm,connection,3,[0., 0.26*1000, 0.19*1000],[-90,-90,0])
+
     scene.Simulation.addChild(scene.Modelling)
 
     return rootNode
