20f99eef5312cddeaddfb4b7c582d0ddc1f53dbf
samFabrice <dsamfabrice5@gmail.com>
2022-12-08 16:40:01 +0100

    add comment on how to get wheel_ray  lx, lz value

--- mobile_trunk_sim/wheels_angles_compute.py
+++ mobile_trunk_sim/wheels_angles_compute.py
@@ -1,8 +1,8 @@
 import numpy as np
 
-wheel_ray = 0.113 # rayan de la roue
-lz = 0.229 #distance entre le centre d'une des roues et le centre du chassis sur l'axe des z
-lx = 0.2355 #distance entre le centre d'une des roues et le centre du chassis sur l'axe des x
+wheel_ray = 0.117 # rayon de la roue
+lz = 0.229 #distance entre le centre d'une des roues et le centre du chassis sur l'axe des z (458/2)
+lx = 0.2355 #distance entre le centre d'une des roues et le centre du chassis sur l'axe des x(142/2 + (614-(2*142))/2)
 
 def twistToWheelsAngularSpeed(wy, vz):
     """
@@ -17,6 +17,7 @@ def twistToWheelsAngularSpeed(wy, vz):
     twist = np.array([vz, 0, wy]) # vx = 0
     twist.shape = (3, 1)
     wheels_angular_speed = np.dot(H, twist) #calculate the angular speed of each wheel
+
     wheels_angular_speed = wheels_angular_speed.flatten().tolist()
     return wheels_angular_speed
 
@@ -27,7 +28,7 @@ def move(WheelsMotors_angles_rest_position, wheels_angular_speed, dt):
     """
     with WheelsMotors_angles_rest_position.writeable() as angles:
         #Make the wheel turn according to their angular speed
-
+        print(angles)
         angles[0] += wheels_angular_speed[0] * dt
         angles[2] += wheels_angular_speed[2] * dt
         angles[1] += wheels_angular_speed[1] * dt
