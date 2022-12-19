0e7bbf2f7b35abbd3314a1c02730f3ec4a38fbda
samFabrice <dsamfabrice5@gmail.com>
2022-12-19 15:49:06 +0100

    Correction of the implemented mathematical model (holonomic robot model) by that of a differential robot model

--- mobile_trunk_sim/wheels_angles_compute.py
+++ mobile_trunk_sim/wheels_angles_compute.py
@@ -10,16 +10,21 @@ def twistToWheelsAngularSpeed(wy, vz):
         twistToWheelsAngles method calculate the angular velocity
             of each of the wheels
     """
-    H = np.array([[1, -1, -(lx+ lz)],
-                [1, 1, (lx + lz)],
-                [1, 1, -(lx + lz)],
-                [1, -1, (lx + lz)]])/wheel_ray
-    twist = np.array([vz, 0, wy]) # vx = 0
-    twist.shape = (3, 1)
-    wheels_angular_speed = np.dot(H, twist) #calculate the angular speed of each wheel
+    # H = np.array([[1, -1, -(lx+ lz)],
+    #             [1, 1, (lx + lz)],
+    #             [1, 1, -(lx + lz)],
+    #             [1, -1, (lx + lz)]])/wheel_ray
+    # twist = np.array([vz, 0, wy]) # vx = 0
+    # twist.shape = (3, 1)
+    # wheels_angular_speed = np.dot(H, twist) #calculate the angular speed of each wheel
 
-    wheels_angular_speed = wheels_angular_speed.flatten().tolist()
-    return wheels_angular_speed
+    # wheels_angular_speed = wheels_angular_speed.flatten().tolist()
+
+    # w2 = w3  = (1/wheel_ray)*(lx * wy + vz)
+    # w1 = w4 = (1/wheel_ray) * (-lx*wy + vz)
+    w2 = w3  = (lx * wy)/wheel_ray + vz/wheel_ray
+    w1 = w4 = (-lx*wy)/wheel_ray + vz/wheel_ray
+    return (w1, w2, w3, w4)
 
 def move(WheelsMotors_angles_rest_position, wheels_angular_speed, dt):
     """
@@ -29,7 +34,8 @@ def move(WheelsMotors_angles_rest_position, wheels_angular_speed, dt):
     with WheelsMotors_angles_rest_position.writeable() as angles:
         #Make the wheel turn according to their angular speed
         angles[0] += wheels_angular_speed[0] * dt
-        angles[2] += wheels_angular_speed[2] * dt
         angles[1] += wheels_angular_speed[1] * dt
+        angles[2] += wheels_angular_speed[2] * dt
         angles[3] += wheels_angular_speed[3] * dt
+    print("------------>", WheelsMotors_angles_rest_position.value)
     return WheelsMotors_angles_rest_position.value
