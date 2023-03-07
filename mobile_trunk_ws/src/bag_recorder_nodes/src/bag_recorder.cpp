ffba34d0b86acbf85affaf907830389de0a789cd
samFabrice <dsamfabrice5@gmail.com>
2023-03-07 13:52:03 +0100

    Update the bag recoder node

--- mobile_trunk_ws/src/bag_recorder_nodes/src/bag_recorder.cpp
+++ mobile_trunk_ws/src/bag_recorder_nodes/src/bag_recorder.cpp
@@ -12,63 +12,98 @@ using std::placeholders::_1;
 class SimpleBagRecorder : public rclcpp::Node
 {
 public:
-  SimpleBagRecorder()
-  : Node("simple_bag_recorder")
-  {
+    SimpleBagRecorder(double vitesse_angulaire=0., double vitesse_lineaire=0.) : Node("simple_bag_recorder"),
+                      m_vitesse_angulaire(vitesse_angulaire), m_vitesse_lineaire(vitesse_lineaire)
+        {
+          // Get parameters from launch file
+          vitesse_angulaire = this->declare_parameter("vitesse_angulaire", 0.0);
+          vitesse_lineaire = this->declare_parameter("vitesse_lineaire", 0.0);
 
-    const std::string base_folder_name = "my_bag";
+          m_vitesse_angulaire = this->get_parameter("vitesse_angulaire").get_value<double>();
+          m_vitesse_lineaire = this->get_parameter("vitesse_lineaire").get_value<double>();
 
-    if (!std::filesystem::exists(base_folder_name)) {
-        std::filesystem::create_directory(base_folder_name);
-    }
+          // Create base directory if it doesn't exist
+          if(!std::filesystem::exists("../../../record_folder"))
+          {
+            std::filesystem::create_directory("../../../record_folder");
+          }
 
-    const rosbag2_cpp::StorageOptions storage_options({base_folder_name, "sqlite3"});
-    const rosbag2_cpp::ConverterOptions converter_options(
-      {rmw_get_serialization_format(),
-       rmw_get_serialization_format()});
+          // If the folder already exist move to the base directory
+          std::filesystem::current_path("../../../record_folder");
 
-    writer_ = std::make_unique<rosbag2_cpp::writers::SequentialWriter>();
+          //Check if the velocities are different from zero bzfor creating the folder
+          if(m_vitesse_angulaire !=0. && m_vitesse_lineaire !=0.)
+          {
+            std::string base_folder_name = "w_" + std::to_string(m_vitesse_angulaire) + "_v_"+
+                                            std::to_string(m_vitesse_lineaire);
 
-    writer_->open(storage_options, converter_options);
+            //create the new folder withthe velocities as forder name
+            if(!std::filesystem::exists(base_folder_name))
+            {
+              std::filesystem::create_directory(base_folder_name);
 
-    writer_->create_topic(
-      {"chatter",
-       "geometry_msgs/msg/Twist",
-       rmw_get_serialization_format(),
-       ""});
+            }
+            else
+            {
+              std::filesystem::remove_all(base_folder_name);
+              std::filesystem::create_directory(base_folder_name);
 
-    subscription_ = create_subscription<geometry_msgs::msg::Twist>(
-      "chatter", 10, std::bind(&SimpleBagRecorder::topic_callback, this, _1));
-  }
+            }
+          
 
-private:
-  void topic_callback(std::shared_ptr<rclcpp::SerializedMessage> msg) const
-  {
-    auto bag_message = std::make_shared<rosbag2_storage::SerializedBagMessage>();
+          const rosbag2_cpp::StorageOptions storage_options({base_folder_name, "sqlite3"});
 
-    bag_message->serialized_data = std::shared_ptr<rcutils_uint8_array_t>(
-      new rcutils_uint8_array_t,
-      [this](rcutils_uint8_array_t *msg) {
-        auto fini_return = rcutils_uint8_array_fini(msg);
-        delete msg;
-        if (fini_return != RCUTILS_RET_OK) {
-          RCLCPP_ERROR(get_logger(),
-            "Failed to destroy serialized message %s", rcutils_get_error_string().str);
+          const rosbag2_cpp::ConverterOptions converter_options(
+                 {rmw_get_serialization_format(),
+                 rmw_get_serialization_format()});   
+
+          writer_ = std::make_unique<rosbag2_cpp::writers::SequentialWriter>();
+
+          writer_->open(storage_options, converter_options);
+
+          writer_->create_topic(
+              {"chatter",
+                   "geometry_msgs/msg/Twist",
+                  rmw_get_serialization_format(),
+                    ""});  
+
+          subscription_ = create_subscription<geometry_msgs::msg::Twist>(
+                "chatter", 10, std::bind(&SimpleBagRecorder::topic_callback, this, _1));          
+        } else{
+                RCLCPP_ERROR(get_logger(), "Invalid velocities");
         }
-      });
-    *bag_message->serialized_data = msg->release_rcl_serialized_message();
 
-    bag_message->topic_name = "chatter";
-    if (rcutils_system_time_now(&bag_message->time_stamp) != RCUTILS_RET_OK) {
-      RCLCPP_ERROR(get_logger(), "Error getting current time: %s",
-        rcutils_get_error_string().str);
+    }
+private:
+    void topic_callback(std::shared_ptr<rclcpp::SerializedMessage> msg) const
+    {
+      auto bag_message = std::make_shared<rosbag2_storage::SerializedBagMessage>();
+
+      bag_message->serialized_data = std::shared_ptr<rcutils_uint8_array_t>(
+        new rcutils_uint8_array_t,
+        [this](rcutils_uint8_array_t *msg) {
+          auto fini_return = rcutils_uint8_array_fini(msg);
+          delete msg;
+          if (fini_return != RCUTILS_RET_OK) {
+            RCLCPP_ERROR(get_logger(),
+              "Failed to destroy serialized message %s", rcutils_get_error_string().str);
+          }
+        });
+
+      *bag_message->serialized_data = msg->release_rcl_serialized_message();
+
+      bag_message->topic_name = "chatter";
+      if (rcutils_system_time_now(&bag_message->time_stamp) != RCUTILS_RET_OK) {
+        RCLCPP_ERROR(get_logger(), "Error getting current time: %s",
+          rcutils_get_error_string().str);
+      }
+
+      writer_->write(bag_message);
     }
 
-    writer_->write(bag_message);
-  }
-
-  rclcpp::Subscription<rclcpp::SerializedMessage>::SharedPtr subscription_;
-  std::unique_ptr<rosbag2_cpp::writers::SequentialWriter> writer_;
+    rclcpp::Subscription<rclcpp::SerializedMessage>::SharedPtr subscription_;
+    std::unique_ptr<rosbag2_cpp::writers::SequentialWriter> writer_;
+    double  m_vitesse_angulaire, m_vitesse_lineaire;
 };
 
 int main(int argc, char * argv[])
@@ -77,4 +112,4 @@ int main(int argc, char * argv[])
   rclcpp::spin(std::make_shared<SimpleBagRecorder>());
   rclcpp::shutdown();
   return 0;
-}
+  }
